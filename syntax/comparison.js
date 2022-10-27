2103522bc5d277467a82ca1513ebc0347333d3f6
hyunmingit <jhmcos13@gmail.com>
2022-10-27 21:29:09 +0900

    221027 nodejs

---
+++ syntax/comparison.js
@@ -0,0 +1,5 @@
+console.log(1>1);
+console.log(1===1);
+console.log(1===2);
+
+name === 1;
---
+++ syntax/loop.js
@@ -0,0 +1,9 @@
+
+var i = 0
+while(i < 100){
+  console.log('C1');
+  console.log('C2');
+  i ++;
+}
+console.log('D');
+console.log(i);
